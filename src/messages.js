9c1f48a507134e063ad9a1d209c864f0cd29e5f0
Ray Lee <rhlee@berkeley.edu>
2018-08-11 13:20:36 -0700

    NOJIRA: Customize login page messages.

--- src/messages.js
+++ src/messages.js
@@ -1,4 +1,51 @@
 export default {
+  'about.title': 'Welcome to the CollectionSpace Materials Authority Demo',
+  'about.contentHTML': `
+    <p>
+      CollectionSpace is a free, open-source collections management application for museums,
+      historical societies, natural science collections, and more.
+    </p>
+    <p>
+      This demo site is running the
+      <a href="http://www.collectionspace.org/current_release/">current version</a> of the
+      <a href="https://wiki.collectionspace.org/x/lYOkCQ">materials authority profile</a> of
+      CollectionSpace. If you’re interested in a version of CollectionSpace configured to meet the
+      needs of a specific domain, please visit our
+      <a href="http://demo.collectionspace.org">demo landing page</a>
+      to view all available options. Examples include Anthropology, Botanical Garden, Fine Art,
+      Herbarium, and Local History &amp; Material Culture.
+    </p>
+    <p>
+      To log in, use the email <strong>admin@materials.collectionspace.org</strong>, with the
+      case-sensitive password <strong>Administrator</strong>. To view in read-only mode, use the
+      email <strong>reader@materials.collectionspace.org</strong> with the password
+      <strong>reader</strong>.
+    </p>
+    <p>
+      Not sure where to get started? Follow along with one of our quick-start guides:
+    </p>
+    <ul>
+      <li>
+        <span>Create a new object:</span>
+        <a href="http://bit.ly/newobjpdf">PDF</a>
+        <a href="https://vimeo.com/112212895">Screencast</a>
+      </li>
+      <li>
+        <span>Link to an image:</span>
+        <a href="http://bit.ly/linkimgpdf">PDF</a>
+        <a href="https://vimeo.com/112214418">Screencast</a>
+      </li>
+      <li>
+        <span>Add a storage location:</span>
+        <a href="http://bit.ly/storlocpdf">PDF</a>
+        <a href="https://vimeo.com/112818120">Screencast</a>
+      </li>
+    </ul>
+    <p>
+      We rebuild this demo site weekly so don't worry, you won't break it!
+    </p>
+  `,
+
   // dimension
 
   'field.ext.dimension.dimensionSummary.name': 'Sample size',
