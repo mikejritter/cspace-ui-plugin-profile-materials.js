ebddc47ce0efdc01ff675e19d180dbe282013853
Ray Lee <rhlee@berkeley.edu>
2018-10-01 21:36:03 -0700

    CSPACE-7160: Rename sample description to production description.

--- src/messages.js
+++ src/messages.js
@@ -55,7 +55,7 @@ export default {
   'field.collectionobjects_common.objectNumber.name': 'Accession number',
   'column.collectionobject.default.objectNumber': 'Accession number',
   'column.collectionobject.narrow.objectNumber': 'Acc. number',
-  'field.collectionobjects_common.briefDescription.name': 'Sample description',
+  'field.collectionobjects_common.briefDescription.name': 'Production description',
   'field.collectionobjects_common.objectStatus.name': 'Object type',
   'field.collectionobjects_common.reference.name': 'Supplementary literature description',
 };
