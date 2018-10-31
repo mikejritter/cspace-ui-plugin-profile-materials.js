eeb98a8a5978ddf145271e6f19dc16eeffaa5811
Ray Lee <rhlee@berkeley.edu>
2018-10-30 20:08:42 -0700

    DRYD-376: Hide media fields in adv search that were hidden in the record form.

--- src/plugins/recordTypes/media/index.js
+++ src/plugins/recordTypes/media/index.js
@@ -1,9 +1,11 @@
+import advancedSearch from './advancedSearch';
 import fields from './fields';
 import forms from './forms';
 
 export default () => configContext => ({
   recordTypes: {
     media: {
+      advancedSearch: advancedSearch(configContext),
       fields: fields(configContext),
       forms: forms(configContext),
     },
