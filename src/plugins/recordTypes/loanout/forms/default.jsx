e1fc2248c29ca8fa43e0455e9a22ef39228135d0
Ray Lee <ray.lee@lyrasis.org>
2019-03-09 00:48:14 -0800

    DRYD-571: Rename loan group/individual fields.

--- src/plugins/recordTypes/loanout/forms/default.jsx
+++ src/plugins/recordTypes/loanout/forms/default.jsx
@@ -35,8 +35,8 @@ const template = (configContext) => {
 
         <Field name="loanStatusGroupList">
           <Field name="loanStatusGroup">
-            <Field name="loanApproverGroup" />
-            <Field name="loanApprover" />
+            <Field name="loanGroup" />
+            <Field name="loanIndividual" />
             <Field name="loanStatus" />
             <Field name="loanStatusDate" />
             <Field name="loanStatusNote" />
