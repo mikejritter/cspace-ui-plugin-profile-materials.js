e12c199aeb25309c60db5d146662954000184dc9
Ray Lee <ray.lee@lyrasis.org>
2020-01-11 13:20:14 -0800

    DRYD-796: Update additional messages for searching on all fields.

--- src/messages.js
+++ src/messages.js
@@ -1,60 +1,17 @@
 export default {
-  'about.title': 'Welcome to the CollectionSpace Materials Authority Demo',
-  'about.contentHTML': `
-    <p>
-      CollectionSpace is a free, open-source collections management application for museums,
-      historical societies, natural science collections, and more.
-    </p>
-    <p>
-      This demo site is running the
-      <a href="http://www.collectionspace.org/current_release/">current version</a> of the
-      <a href="https://collectionspace.atlassian.net/wiki/x/CoBDK">materials authority profile</a> of
-      CollectionSpace. If you’re interested in a version of CollectionSpace configured to meet the
-      needs of a specific domain, please visit our
-      <a href="https://www.collectionspace.org/demo/">demo landing page</a>
-      to view all available options. Examples include Anthropology, Botanical Garden, Fine Art,
-      Herbarium, and Local History &amp; Material Culture.
-    </p>
-    <p>
-      To log in, use the email <strong>admin@materials.collectionspace.org</strong>, with the
-      case-sensitive password <strong>Administrator</strong>. To view in read-only mode, use the
-      email <strong>reader@materials.collectionspace.org</strong> with the password
-      <strong>reader</strong>.
-    </p>
-    <p>
-      Not sure where to get started? Follow along with one of our quick-start guides:
-    </p>
-    <ul>
-      <li>
-        <span>Create a new object:</span>
-        <a href="http://bit.ly/newobjpdf">PDF</a>
-        <a href="https://vimeo.com/112212895">Screencast</a>
-      </li>
-      <li>
-        <span>Link to an image:</span>
-        <a href="http://bit.ly/linkimgpdf">PDF</a>
-        <a href="https://vimeo.com/112214418">Screencast</a>
-      </li>
-      <li>
-        <span>Add a storage location:</span>
-        <a href="http://bit.ly/storlocpdf">PDF</a>
-        <a href="https://vimeo.com/112818120">Screencast</a>
-      </li>
-    </ul>
-    <p>
-      We rebuild this demo site weekly so don't worry, you won't break it!
-    </p>
-  `,
+  'about.title': 'Welcome to CollectionSpace: Materials Authority',
 
   // dimension
 
+  'field.ext.dimension.dimensionSummary.fullName': 'Sample size',
   'field.ext.dimension.dimensionSummary.name': 'Sample size',
 
   // collectionobject
 
-  'field.collectionobjects_common.objectNumber.name': 'Accession number',
   'column.collectionobject.default.objectNumber': 'Accession number',
   'column.collectionobject.narrow.objectNumber': 'Acc. number',
+
+  'field.collectionobjects_common.objectNumber.name': 'Accession number',
   'field.collectionobjects_common.briefDescription.name': 'Production description',
   'field.collectionobjects_common.objectStatus.name': 'Object type',
   'field.collectionobjects_common.reference.name': 'Supplementary literature description',
@@ -62,6 +19,6 @@ export default {
   // loanout
 
   'field.loansout_common.lendersAuthorizer.name': 'Lender authorizer',
-  'field.loansout_common.borrower.name': 'Borrower',
   'field.loansout_common.borrower.fullName': 'Borrower',
+  'field.loansout_common.borrower.name': 'Borrower',
 };
