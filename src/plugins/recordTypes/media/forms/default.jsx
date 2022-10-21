ff3fcbdc0deb09d6e8f4543d969e2d4073eb2477
Michael Ritter <emericaskate15@gmail.com>
2022-10-21 16:25:35 -0600

    DRYD-1035: Add checksum to media handling

--- src/plugins/recordTypes/media/forms/default.jsx
+++ src/plugins/recordTypes/media/forms/default.jsx
@@ -36,6 +36,14 @@ const template = (configContext) => {
 
         <Field name="externalUrl" />
 
+        <Field name="checksumGroupList">
+          <Field name="checksumGroup">
+            <Field name="checksumValue" />
+            <Field name="checksumType" />
+            <Field name="checksumDate" />
+          </Field>
+        </Field>
+
         <Cols>
           <Col>
             <Field name="contributor" />
