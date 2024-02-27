794439867b4fcf85a3078d7aa01547d9a18cad90
Michael Ritter <emericaskate15@gmail.com>
2024-02-27 14:42:50 -0700

    Hide associated authorities from place

--- src/plugins/recordTypes/place/index.js
+++ src/plugins/recordTypes/place/index.js
@@ -1,10 +1,12 @@
 import fields from './fields';
+import forms from './forms';
 import vocabularies from './vocabularies';
 
 export default () => (configContext) => ({
   recordTypes: {
     place: {
       vocabularies,
+      forms: forms(configContext),
       fields: fields(configContext),
     },
   },
