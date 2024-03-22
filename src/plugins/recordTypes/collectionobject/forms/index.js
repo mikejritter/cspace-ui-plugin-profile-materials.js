193477fb7b8f25a856c15d0f839a7849543fd66b
Michael Ritter <mikejritter@users.noreply.github.com>
2024-03-21 19:53:12 -0600

    DRYD-1403: Use 7.2 publicbrowser template (#15)

--- src/plugins/recordTypes/collectionobject/forms/index.js
+++ src/plugins/recordTypes/collectionobject/forms/index.js
@@ -1,9 +1,11 @@
 import defaultForm from './default';
 import miniForm from './mini';
+import publicForm from './public';
 
 export default (configContext) => ({
   default: defaultForm(configContext),
   mini: miniForm(configContext),
+  public: publicForm(configContext),
   inventory: {
     disabled: true,
   },
